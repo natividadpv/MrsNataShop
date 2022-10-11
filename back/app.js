15a75d1e97294ac046a7ea9710700edb3cbebc83
Natividad Pedraza <natividadpedraza@gmail.com>
2022-10-11 08:18:44 -0500

    Segundo Commit
    No funciona en postman ni en web error: Error: connect ECONNREFUSED 127.0.0.1:4000

--- back/app.js
+++ back/app.js
@@ -1,4 +1,12 @@
 const express=require("express");
 const app = express();
 
+app.use(express.json());
+
+//Importar rutas
+const productos=require("./routes/products")
+
+//Ruta del navegador
+app.use('/api',productos) //sujeto a cambios
+
 module.exports=app
--- back/server.js
+++ back/server.js
@@ -1,1 +1,9 @@
-const app=require("app")
+const app=require("./app")
+
+//SETEAR EL ARCHIVO DE CONFIGURACION
+const dotenv=require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+const server=app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
