8f789794d62dbea3ded538b1d74a30ce7a4a91f6
Natividad Pedraza <natividadpedraza@gmail.com>
2022-10-13 08:25:46 -0500

    Quinto commit
    Se instala por terminal comandos de REAT como framework para interfaz grafica. Se crea carpeta para HTML (mugre atomo)

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>TIENDA VIRTUAL</h1>
+        <img src="./images/mrsnata_logo.png" alt="Aqui el logo" width="1000" height="600"></img>
+    </div>
+  );
+}
+
+export default App;
